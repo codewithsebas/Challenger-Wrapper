d045b6b7b48676b9732924e74a15670c9d99b95f
Joab Giraldo <joabgiraldo@gmail.com>
2023-03-01 20:27:50 -0500

    Changes in files CSS Styles and Component Header

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+export default function Header() {
+    return (
+        <header>
+            <div className="image">
+                <img src="/Vector.svg" alt="Logo" />
+            </div>
+            <ul className="menu">
+                <li>How We Work</li>
+                <li>Services</li>
+                <li>Free Quote</li>
+                <li>Contact</li>
+            </ul>
+        </header>
+    )
+}
