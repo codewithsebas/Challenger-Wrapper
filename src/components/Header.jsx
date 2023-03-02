c99cf9679de8887cd22b92efd047783c84e3eb5f
Joab Giraldo <joabgiraldo@gmail.com>
2023-03-02 16:12:18 -0500

    Menu mobile and @Media queries Responsive

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,41 @@
+import menu from "../assets/menu.svg";
+import close from "../assets/close.svg";
+import { useState } from "react";
+
 export default function Header() {
-    return (
-        <header>
+  const [slideMenu, setSlideMenu] = useState(false);
+  return (
+    <header>
+      <div className="logo">
+        <img src="/Vector.svg" alt="Logo" />
+      </div>
+      <ul className="menu">
+        <li>How We Work</li>
+        <li>Services</li>
+        <li>Free Quote</li>
+        <li>Contact</li>
+      </ul>
+      <div onClick={() => setSlideMenu(true)} className="menuMobile">
+        <img src={menu} alt="Menu" />
+      </div>
+      <div className={`${slideMenu ? "backClose" : "backCloseSlide"}`}>
+        <div className={`${slideMenu ? "menuSlide" : " menuSlideClose"}`}>
+          <div className="top">
             <div className="logo">
-                <img src="/Vector.svg" alt="Logo" />
+              <img src="/Vector.svg" alt="Logo" />
             </div>
-            <ul className="menu">
-                <li>How We Work</li>
-                <li>Services</li>
-                <li>Free Quote</li>
-                <li>Contact</li>
-            </ul>
-        </header>
-    )
+            <div className="close" onClick={() => setSlideMenu(false)}>
+              <img src={close} alt="Logo" />
+            </div>
+          </div>
+          <div className="mobile">
+            <li>How We Work</li>
+            <li>Services</li>
+            <li>Free Quote</li>
+            <li>Contact</li>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
 }
